cd2a2f16352636711becdf548b3c345ee6f135f3
SamSpellman <sspellman2@wisc.edu>
2023-02-04 15:36:28 -0600

    Trying to push to remote origin

---
+++ searchAndSort/BinarySearch.java
@@ -0,0 +1,21 @@
+/**
+ * Author: Samuel Spellman
+ *
+ * This will be a static class that will perform a binary search on valid lists
+ * or iterable, comparable objects.
+ *
+ **/
+
+public static class BinarySearch<T extends Iterable<T>> () {
+	// TODO: Search for an item the data given.
+	// Uses bounded generics to be able to iterate through type passed
+	T toSearchThrough = new T;
+
+	public static void main (String[] args)
+	{
+		// TODO: Write tester methods to check the binary search
+		System.out.println("Testing, Testing");
+	}	
+	
+
+}
